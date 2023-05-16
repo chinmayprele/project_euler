130591124a3778dbd57d239ef08e4878d9573f45
Chinmay P. Rele <26606959+drosophominin@users.noreply.github.com>
2023-05-16 08:37:03 -0500

    added multithreading and corrected time-intensive is_prime() function

--- 0010-summation_of_primes/problem-0010.md
+++ 0010-summation_of_primes/problem-0010.md
@@ -8,10 +8,41 @@ Find the sum of all the primes below two million.
 
 1. `142913828923` &larr; Incorrect
 	- Did not account for _1_
-1. `142913828922` &larr; Correct
+2. `142913828922` &larr; Correct
 
 ## Notes
 
 - As the numbers during the iteration increase, the `is_prime()` function is taking longer and longer to run.
 	- A solution would have been to multithread each of these numbers to run on individual cores so that many numbers can be computed as primes at once.
 	- An alternate solution would also be to identiefy from online the list of primes from a database, but that isn't fun.
+
+Though it was possible to multithread the output, I intentified the following issues:
+
+- The following function was too slow since it went through too many iterations.
+	```Python
+	def is_prime( number ):
+	for i in range( 2, number ):
+		if number % i == 0:
+			return( False )
+	if number <= 1:
+		return( False )
+	return( True )
+	```
+- Instead, the following function was much quicker since it:
+	1. Only went upto the square root of the number,
+	2. Skipped checking multiples of _2_, and 
+	3. Checked for evenness early on during validation.
+	```Python
+	def is_prime(n):
+	if (n < 2) or (n % 2 == 0 and n > 2):
+		return False
+	elif n == 2:
+		return True
+	elif n == 3:
+		return True
+	else:
+		for i in range(3, math.ceil(math.sqrt(n)) + 1, 2):
+			if n % i == 0:
+				return False
+		return True
+	```
--- 0010-summation_of_primes/problem-0010.py
+++ 0010-summation_of_primes/problem-0010.py
@@ -1,17 +1,147 @@
 from tqdm import tqdm as tqdm
+import time
+import math
+from multiprocessing import Pool
+from multiprocessing import freeze_support
+import sys
 
-def is_prime( number ):
-	for i in range( 2, number ):
-		if number % i == 0:
-			return( False )
-	if number <= 1:
-		return( False )
-	return( True )
+
+# def run_multiprocessing(func, i, n_processors):
+# 	'''
+# 	Define function to run mutiple processors and pool the results together
+# 	'''
+# 	with Pool(processes=n_processors) as pool:
+# 		return pool.map(func, i)
+
+# def is_prime( number ):
+# 	for i in range( 2, number ):
+# 		if number % i == 0:
+# 			return( False )
+# 	if number <= 1:
+# 		return( False )
+# 	return( True )
+
+def is_prime(n):
+	if (n < 2) or (n % 2 == 0 and n > 2):
+		return False
+	elif n == 2:
+		return True
+	elif n == 3:
+		return True
+	else:
+		for i in range(3, math.ceil(math.sqrt(n)) + 1, 2):
+			if n % i == 0:
+				return False
+		return True
 
 rolling_sum = 0
 
-for i in tqdm( range( 2, int(10)+1 ) ):
+for i in tqdm( range( 2, int(2e6)+1 ) ):
 	if is_prime( i ):
 		rolling_sum += i
 
 print( rolling_sum )
+
+sys.exit( "only needed to change function; following multothreading works" )
+
+# def main():
+# 	start = time.perf_counter()
+
+# 	'''
+# 	set up parameters required by the task
+# 	'''
+# 	num_max = int(2e6)+1
+# 	n_processors = 6
+# 	x_ls = list(range(1, num_max))
+
+# 	'''
+# 	pass the task function, followed by the parameters to processors
+# 	'''
+# 	out = run_multiprocessing(is_prime, x_ls, n_processors)
+
+# 	print("Input length: {}".format(len(x_ls)))
+# 	print("Output length: {}".format(len(out)))
+# 	print("Mutiprocessing time: {}mins\n".format((time.perf_counter()-start)/60))
+# 	print("Mutiprocessing time: {}secs\n".format((time.perf_counter()-start)))
+
+
+# if __name__ == "__main__":
+# 	freeze_support()   # required to use multiprocessing
+# 	main()
+
+# prime_mutiprocessing.py
+
+import time
+import math
+from multiprocessing import Pool
+from multiprocessing import freeze_support
+
+
+'''Define function to run mutiple processors and pool the results together'''
+def run_multiprocessing(func, i, n_processors):
+	with Pool(processes=n_processors) as pool:
+		return pool.map(func, i)
+
+'''Define task function'''
+def is_prime(n):
+	if (n < 2) or (n % 2 == 0 and n > 2):
+		return False
+	elif n == 2:
+		return True
+	elif n == 3:
+		return True
+	else:
+		for i in range(3, math.ceil(math.sqrt(n)) + 1, 2):
+			if n % i == 0:
+				return False
+		return True
+
+# def is_prime( number ):
+# 	'''
+# 	This function is TOO SLOW.
+# 	'''
+# 	for i in range( 2, number ):
+# 		if number % i == 0:
+# 			return( False )
+# 	if number <= 1:
+# 		return( False )
+# 	return( True )
+
+def find_indices( list_to_check, item_to_find ):
+	indices = []
+	for idx, value in enumerate(list_to_check):
+		if value == item_to_find:
+			indices.append( idx + 1 )
+	return indices
+
+def main():
+	start = time.perf_counter()
+
+	'''
+	set up parameters required by the task
+	'''
+	num_max = int( 2e6 + 1 )
+	n_processors = 6
+	x_ls = list(range(1, num_max))
+
+	'''
+	pass the task function, followed by the parameters to processors
+	'''
+	prime_bool = run_multiprocessing(is_prime, x_ls, n_processors)
+
+	print("Input length: {}".format(len(x_ls)))
+	print("Output length: {}".format(len(prime_bool)))
+	print("Mutiprocessing time: {}mins\n".format((time.perf_counter()-start)/60))
+	print("Mutiprocessing time: {}secs\n".format((time.perf_counter()-start)))
+
+	nums = find_indices( prime_bool, True )
+
+	print( nums[:5] )
+
+
+if __name__ == "__main__":
+	freeze_support()   # required to use multiprocessing
+	main()
+
+
+
